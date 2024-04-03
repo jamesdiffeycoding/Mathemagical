c028bf81915ccb236753a2d37df43a3cfa7ce810
James <jamesdiffey8@gmail.com>
2024-04-03 17:36:15 +0100

    complete header

--- Mathereactical-project/src/App.tsx
+++ Mathereactical-project/src/App.tsx
@@ -1,20 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import './App.css'
 import Graph from './components/Graph.jsx'
 import Footer from './components/Footer.jsx'
-
-import './App.css'
+import Header from './components/Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div>    
+        <Header></Header>    
+        <hr></hr>
+
         <Graph></Graph>
+        <hr></hr>
         <Footer></Footer>
+        <hr></hr>
       </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
---
+++ Mathereactical-project/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect} from 'react';
+import reactLogo from '../assets/react.svg'
+import './header.css'
+export default function Header() {
+
+  return (
+    <section>
+      <div className="header-grid">
+        {/* 1 */}
+        <div className="header-grid-item react-logo">
+          <a href="https://github.com/jamesdiffeycoding" target="_blank"> <img src={reactLogo} className="logo react" alt="React logo" /> </a>
+        </div>
+        {/* 2 */}
+        <div className="header-grid-item project-name codystar-regular">
+          <h1>MatheMagical</h1>
+        </div>
+        {/* 3 */}
+        <div className="header-grid-item subtitle codystar-light">
+          <p>
+            Aesthetic Math
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
